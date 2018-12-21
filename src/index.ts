aac0b682118ead00cbf153d57a4872d3c00eafa1
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-12-21 01:37:48 -0500

    updates multi-rpc

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as FaceManagementService } from "./FaceManagementService";
+export { default as CommandService } from "./CommandService";
+export { default as DetectionService } from "./DetectionService";
+export { default as AppResources } from "./AppResources";
+export { default as ConfigService } from "./ConfigService";
